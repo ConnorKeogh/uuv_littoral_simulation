f8f7f48c853fed9793f719fa80825e3fcdc75bac
ConnorKeogh <connor.keogh@student.uts.edu.au>
2024-05-26 17:40:28 +1000

    clean initial push

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ck/catkin_ws/src/uuv_littoral_simulation
+# Build directory: /home/ck/catkin_ws/src/uuv_littoral_simulation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("models")
+subdirs("worlds")
---
+++ worlds/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ck/catkin_ws/src/uuv_littoral_simulation/worlds
+# Build directory: /home/ck/catkin_ws/src/uuv_littoral_simulation/worlds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
